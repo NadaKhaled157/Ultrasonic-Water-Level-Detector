8a6ad5dba46abfcc267752684f917eb35dbe8f27
NadaKhaled157 <124778473+NadaKhaled157@users.noreply.github.com>
2023-03-25 13:39:03 +0200

    Add files via upload

---
+++ Ultrasonic_Water_Level_Detector_Code.ino
@@ -0,0 +1,70 @@
+#define echoPin 8 //connect echo pin of ultrasonic sensor to D8 of Arduino
+#define trigPin 7 //connect trigger pin of ultrasonic sensor to D7 of Arduino
+int redLed=10;
+int yellowLed=11;
+int greenLed=12;
+int buzzer=13;
+long duration; // declare variables to hold duration and distance
+long distance;
+void setup() //setup() is used for initialization
+{
+Serial.begin(9600); //set the baud rate of serial communication to 9600
+pinMode(trigPin,OUTPUT); //set trigPin as output pin of Arduino
+pinMode(echoPin,INPUT); //set echoPin as output pin of Arduino
+pinMode(redLed,OUTPUT);
+pinMode(yellowLed,OUTPUT);
+pinMode(greenLed,OUTPUT);
+pinMode(buzzer,OUTPUT);
+}
+void loop(){
+digitalWrite(trigPin,LOW); //generate square wave at trigger pin
+delayMicroseconds(2);
+digitalWrite(trigPin,HIGH);
+delayMicroseconds(10);
+digitalWrite(trigPin,LOW);
+duration=pulseIn(echoPin,HIGH);
+distance=340*duration/20000;
+// pulseIn is used to Reads a pulse (either HIGH or LOW) on a pin.
+//For example, if value is HIGH, pulseIn() waits for the pin to
+//go HIGH, starts timing, then waits for the pin to go LOW and stops timing.
+//Returns the length of the pulse in microseconds
+Serial.println(distance);
+delay(500);
+if (distance>=0 && distance<3 ){
+digitalWrite(greenLed,HIGH);
+digitalWrite(yellowLed,LOW);
+digitalWrite(redLed,LOW);
+digitalWrite(buzzer,LOW);
+Serial.println("The tank is full.");
+delay(500);
+}
+else if(distance>=3 && distance<6)
+{
+digitalWrite(yellowLed,HIGH);
+digitalWrite(greenLed,LOW);
+digitalWrite(redLed,LOW);
+digitalWrite(buzzer,LOW);
+Serial.println("The tank is half-full.");
+delay(500);
+
+}
+else if(distance>=6 && distance<=9) {
+digitalWrite(redLed,HIGH);
+digitalWrite(yellowLed,LOW);
+digitalWrite(greenLed,LOW);
+tone(buzzer,100);
+delay(500);
+noTone(buzzer);
+delay(20);
+Serial.println("The tank is empty.");
+delay(500);
+} else if (distance>10) {
+digitalWrite(redLed,LOW);
+digitalWrite(yellowLed,LOW);
+digitalWrite(greenLed,LOW);
+noTone(buzzer);
+Serial.println("The sensor is not attached to a tank.");
+delay (500);
+}
+delay(500);
+}
